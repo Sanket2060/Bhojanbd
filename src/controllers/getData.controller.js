2dcbdb9c01b65edc58716c251b52e150aa08ea5e
Sanket <sanketkarki2060@gmail.com>
2024-02-18 08:26:08 +0545

    feat🪶:Added getAllCompletedOrdersForDonor controller

--- src/controllers/getData.controller.js
+++ src/controllers/getData.controller.js
@@ -59,4 +59,43 @@ const showActiveOrders=asyncHandler(async function(req,res){
     .status(200)
     .json(new ApiResponse(200,result?{result}:null,"Active Orders sent successfully"))
   })
-export {getTopDistributors,getTopDonors,getUserDetailsFromName,showActiveOrders}
+  const getAllCompletedOrdersForDonor = asyncHandler(async (req, res) => {
+    const { _id } = req.body;
+
+    try {
+        // Find the donor by _id
+        const user = await Donor.findOne({ _id }).select("-password -refreshToken");
+
+        // If no user found with the provided _id, throw an error
+        if (!user) {
+            throw new ApiError(401, "Invalid _id. No user with this id");
+        }
+        console.log("User:", user);
+
+        // Extract completed orders for the user
+        let completedOrders = [];
+
+        if (user.order && user.order.length > 0) {
+            const allUsers = await Promise.all(user.order.map(async (item) => {
+                const theOrder = await Order.findById(item);
+                return theOrder;
+            }));
+
+            completedOrders = allUsers.filter(order => !order.isActive && order.orderStatus == "completed");
+        }
+        console.log("completedOrders:", completedOrders);
+
+        res.status(200).json(new ApiResponse(200, { completedOrders }, "Completed orders for donor sent successfully"));
+    } catch (error) {
+        console.log("Error in retrieving completed orders for donor:", error);
+        throw new ApiError(500, "Internal Server Error");
+    }
+});
+
+export {
+    getTopDistributors,
+    getTopDonors,
+    getUserDetailsFromName,
+    showActiveOrders,
+    getAllCompletedOrdersForDonor
+};
--- src/routes/getData.routes.js
+++ src/routes/getData.routes.js
@@ -1,10 +1,12 @@
 //add getData route
 import { Router } from "express";
-import { getTopDistributors, getTopDonors, getUserDetailsFromName,showActiveOrders } from "../controllers/getData.controller.js";
+import { getTopDistributors, getTopDonors, getUserDetailsFromName,showActiveOrders,getAllCompletedOrdersForDonor } from "../controllers/getData.controller.js";
 
 const router=Router();
 router.route('/get-top-donors').get(getTopDonors)
 router.route('/get-top-distributors').get(getTopDistributors);
 router.route('/getdetailsfromname').post(getUserDetailsFromName);
 router.route('/active-listings').get(showActiveOrders);
+router.route('/getAllCompletedOrdersForDonor').post(getAllCompletedOrdersForDonor);
+
 export default router
