b5038f702387d5b171f6289336b4f0d38ada7eda
Sanket <sanketkarki2060@gmail.com>
2024-05-30 11:59:48 +0545

    feat:added controller for finding rank of users

--- src/app.js
+++ src/app.js
@@ -1,14 +1,26 @@
 import  express  from "express";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 const app=express();
+const server = createServer(app);
+// const io = new Server(server);
 // app.use(cors({
 //     origin: process.env.CORS_ORIGIN,   //providing cors to frontend
 //     credentials:true
 
 // }))
+// Initialize Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
 app.use(cors({
-  origin:"https://khana.me",
+  origin:"http://localhost:5173",
   credentials:true
 }))
 app.use(express.json({limit:"16kb"}))
@@ -27,4 +39,29 @@ app.use('/api/v1/order',orderRouter);
 
 import getData from './routes/getData.routes.js'
 app.use('/api/v1/getData',getData)
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
+// Socket.IO connection event
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  // Emit a notification when a user connects
+  socket.emit('notification', 'Welcome to the app!');
+
+  // Handle custom events
+  socket.on('customEvent', (data) => {
+    console.log(data);
+    // Handle the event
+  });
+
+  // Handle disconnect
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 export {app}
--- src/routes/getData.routes.js
+++ src/routes/getData.routes.js
@@ -1,6 +1,6 @@
 //add getData route
 import { Router } from "express";
-import { getOrganizationDetails,getTopDistributors, getTopDonors, getUserDetailsFromName,showActiveOrders,getAllCompletedOrdersForDonor,getUserDetails } from "../controllers/getData.controller.js";
+import {getDistributorsRank,getDonorsRank,getOrganizationDetails,getTopDistributors, getTopDonors, getUserDetailsFromName,showActiveOrders,getAllCompletedOrdersForDonor,getUserDetails } from "../controllers/getData.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router=Router();
 router.route('/get-top-donors').get(getTopDonors)
@@ -10,5 +10,7 @@ router.route('/active-listings').get(showActiveOrders);
 router.route('/getOrganizationDetails').get(getOrganizationDetails);
 router.route('/getUserDetails').get(verifyJWT,getUserDetails);
 router.route('/getAllCompletedOrdersForDonor').post(getAllCompletedOrdersForDonor);
+router.route('/getDonorsRank').post(getDonorsRank);
+router.route('/getDistributorsRank').post(getDistributorsRank);
 
 export default router
