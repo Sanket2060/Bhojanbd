9e18b148719c4b8b9e6dc5391b6cedb96e994ec5
Sanket <sanketkarki2060@gmail.com>
2024-06-02 07:47:51 +0545

    updated cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import  express  from "express";
+import express from "express";
 import cookieParser from "cookie-parser";
-import cors from 'cors'
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-const app=express();
+import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
+const app = express();
 const server = createServer(app);
 // const io = new Server(server);
 // app.use(cors({
@@ -19,26 +19,27 @@ const server = createServer(app);
 //     credentials: true
 //   }
 // });
-app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true
-}))
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({limit:"16kb",extended:true}))
+app.use(
+  cors({
+    origin: "https://khana.me",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ limit: "16kb", extended: true }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-
 //routes import (done here so that to differentiate it from others)
-import userRouter from './routes/user.routes.js'
-app.use('/api/v1/users',userRouter)  //reach to users route on /users
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter); //reach to users route on /users
 //url:https//:localhost:9005/api/v1/users/register
 
-import orderRouter from './routes/order.routes.js'  //afaile naam rakhne ho(default export vayesi)
-app.use('/api/v1/order',orderRouter);
+import orderRouter from "./routes/order.routes.js"; //afaile naam rakhne ho(default export vayesi)
+app.use("/api/v1/order", orderRouter);
 
-import getData from './routes/getData.routes.js'
-app.use('/api/v1/getData',getData)
+import getData from "./routes/getData.routes.js";
+app.use("/api/v1/getData", getData);
 
 // app.use((req, res, next) => {
 //   req.io = io;
@@ -64,4 +65,4 @@ app.use('/api/v1/getData',getData)
 //   });
 // });
 
-export {app}
+export { app };
